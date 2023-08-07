666290051f9c3ab665d745e2e398f63590fd2243
MaxyCopeland <max.copeland1@gmail.com>
2023-08-07 14:15:47 -0400

    added components

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -4,7 +4,7 @@ import { Avatar, ListItem } from "react-native-elements";
 const DirectoryScreen = (props) => {
     const renderDirectoryItem = ({ item: campsite }) => {
         return (
-            <ListItem>
+            <ListItem onPress={() => props.onPress(campsite.id)}>
                 <Avatar source={campsite.image} rounded />
                 <ListItem.Content>
                     <ListItem.Title>{campsite.name}</ListItem.Title>
@@ -13,8 +13,8 @@ const DirectoryScreen = (props) => {
                     </ListItem.Subtitle>
                 </ListItem.Content>
             </ListItem>
-        )
-    }
+        );
+    };
 
     return(
         <FlatList 
