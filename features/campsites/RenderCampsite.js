b1c6227eb042b2d765b1746f8500b70dd45e1e44
MaxyCopeland <max.copeland1@gmail.com>
2023-08-23 14:27:53 -0400

    in process of adding modal

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,6 +1,7 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Modal } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import { baseUrl } from '../../shared/baseUrl';
+import { useState } from 'react';
 
 const RenderCampsite = (props) => {
     const { campsite } = props;
@@ -9,30 +10,30 @@ const RenderCampsite = (props) => {
             <Card containerStyle={styles.cardContainer}>
                 <Card.Image source={{ uri: baseUrl + campsite.image }}>
                     <View style={{ justifyContent: 'center', flex: 1 }}>
+                    
+                    {/* made edits here */}
                         <Text
-                            style={{
-                                color: 'white',
-                                textAlign: 'center',
-                                fontSize: 20
-                            }}
+                            style={styles.cardText}
                         >
                             {campsite.name}
                         </Text>
                     </View>
                 </Card.Image>
                 <Text style={{ margin: 20 }}>{campsite.description}</Text>
-                <Icon
-                    name={props.isFavorite ? 'heart' : 'heart-o'}
-                    type='font-awesome'
-                    color='#f50'
-                    raised
-                    reverse
-                    onPress={() =>
-                        props.isFavorite
-                            ? console.log('Already set as a favorite')
-                            : props.markFavorite()
-                    }
-                />
+                    <View style={styles.cardRow}>
+                        <Icon style={styles.icon}/>
+                    </View>
+
+                <cardRow style={styles.cardRow}>
+                    <View>
+                    </View>
+                </cardRow>  
+                
+                <cardText style={styles.cardText}>
+                    <View>
+                    </View>
+                </cardText>
+
             </Card>
         );
     }
@@ -40,11 +41,36 @@ const RenderCampsite = (props) => {
 };
 
 const styles = StyleSheet.create({
-    cardContainer: {
-        padding: 0,
-        margin: 0,
-        marginBottom: 20
+        cardContainer: {
+            padding: 0,
+            margin: 0,
+            marginBottom: 20
+        }
+        cardRow {
+            alignItems:'center',
+            justifyContent:'center',
+            flex: 1,
+            flexDirection: 'row',
+            margin: 20
+        }
+        cardText {
+            textShadowColor: 'rgba(0, 0, 0, 1)',
+            textShadowOffset:{ width: -1, height: 1 },
+            textShadowRadius: 20,
+            textAlign: 'center',
+            color: 'white',
+            fontSize: 20
+        }
+        icon {
+            name:{ props.onShowModal ? 'pencil' },
+            type:'font-awesome',
+            color:'#5637DD',
+            raised,
+            reverse,
+            onShowModal={() => setShowModal(!showModal)}
+        }
     }
-});
+);
 
 export default RenderCampsite;
+
--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,13 +1,23 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, Button, Modal } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import RenderCampsite from '../features/campsites/RenderCampsite';
 import { toggleFavorite } from '../features/favorites/favoritesSlice';
+import { useState } from 'react';
+import ReservationScreen from './ReservationScreen';
+
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
     const comments = useSelector((state) => state.comments);
     const favorites = useSelector((state) => state.favorites);
     const dispatch = useDispatch();
+    const [showModal, setShowModal] = useState(false);
+
+
+    const onShowModal = () => {
+        setShowModal(!showModal);
+    };
+
 
     const renderCommentItem = ({ item }) => {
         return (
@@ -22,29 +32,52 @@ const CampsiteInfoScreen = ({ route }) => {
     };
 
     return (
-        <FlatList
-            data={comments.commentsArray.filter(
-                (comment) => comment.campsiteId === campsite.id
-            )}
-            renderItem={renderCommentItem}
-            keyExtractor={(item) => item.id.toString()}
-            contentContainerStyle={{
-                marginHorizontal: 20,
-                paddingVertical: 20
-            }}
-            ListHeaderComponent={
-                <>
-                    <RenderCampsite
-                        campsite={campsite}
-                        isFavorite={favorites.includes(campsite.id)}
-                        markFavorite={() => dispatch(toggleFavorite(campsite.id))}
-                    />
-                    <Text style={styles.commentsTitle}>Comments</Text>
-                </>
-            }
-        />
+        <>
+            <FlatList
+                data={comments.commentsArray.filter(
+                    (comment) => comment.campsiteId === campsite.id
+                )}
+                renderItem={renderCommentItem}
+                keyExtractor={(item) => item.id.toString()}
+                contentContainerStyle={{
+                    marginHorizontal: 20,
+                    paddingVertical: 20
+                }}
+                ListHeaderComponent={
+                    <>
+                        <RenderCampsite
+                            campsite={campsite}
+                            isFavorite={favorites.includes(campsite.id)}
+                            markFavorite={() => dispatch(toggleFavorite(campsite.id))}
+                            onShowModal={onShowModal} 
+                        />
+                        <Text style={styles.commentsTitle}>Comments</Text>
+                    </>
+                }
+            />
+            // ADDED CODE BELOW
+                <Modal
+                    animationType='slide'
+                    transparent={false}
+                    visible={showModal}
+                >
+                    <View style={styles.modal}>
+                            
+                    </View>
+                    <View style={{margin:10}}>
+                        <Button
+                            onPress={() => {
+                                setShowModal(!showModal);
+                            }}
+                            color='#808080'
+                            title='Cancel'
+                        />
+                    </View>
+                </Modal>
+        </>
     );
 };
+
 
 const styles = StyleSheet.create({
     commentsTitle: {
@@ -60,6 +93,10 @@ const styles = StyleSheet.create({
         paddingVertical: 10,
         paddingHorizontal: 20,
         backgroundColor: '#fff'
+    },
+    modal:  {
+        justifyContent: 'center',
+        margin: 20,
     }
 });
 
